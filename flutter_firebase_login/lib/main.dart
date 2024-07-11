772fbe1c33c832d2f3b37532e686c09b7c91ebbb
omer358 <omermaki358@gmail.com>
2024-07-12 03:58:52 +0400

    insure initialize firebase app

--- flutter_firebase_login/lib/main.dart
+++ flutter_firebase_login/lib/main.dart
@@ -5,9 +5,13 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_firebase_login/app/view/app.dart';
 
 import 'app/app_bloc_observer.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = const AppBlocObserver();
 
   await Firebase.initializeApp();
