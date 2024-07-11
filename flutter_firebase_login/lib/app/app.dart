9a60aeabcbbee6cfd060c60e06c495116705e7ac
omer358 <omermaki358@gmail.com>
2024-07-11 16:41:23 +0400

    finish flutter login

---
+++ flutter_firebase_login/lib/app/app.dart
@@ -0,0 +1,4 @@
+export 'view/view.dart';
+export 'bloc/app_bloc.dart';
+export 'routes/routes.dart';
+export 'app_bloc_observer.dart';
---
+++ flutter_firebase_login/packages/form_inputs/lib/form_inputs.dart
@@ -0,0 +1,7 @@
+/// A Very Good Project created by Very Good CLI.
+library;
+
+export 'src/confirm_password.dart';
+export 'src/email.dart';
+export 'src/form_inputs.dart';
+export 'src/password.dart';
